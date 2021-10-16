4625e581df3c982fb1d9bfa236cff8b62ead506e
Thomas MAYNADIE <54234406+Tomenol@users.noreply.github.com>
2021-10-16 17:58:01 +0200

    Delete src directory

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-import EVS.evs_core as evs_core
-import EVS.evs_cosmology as evs_cosmology
-import EVS.evs_wrapper as evs_wrapper
-import EVS.evs_threading as evs_threading
-import EVS.evs_computation as evs_computation
